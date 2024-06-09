acddc687d9a99496c401e8f261a37d41ea8368a7
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-06-09 19:26:46 +0900

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dt_utils",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    url="https://github.com/cm-hirano-shigetoshi/dt-utils-python",
+    author="cm-hirano.shigetoshi",
+    author_email="hirano.shigetoshi@classmethod.jp",
+    description="My utils for datetime.",
+)
